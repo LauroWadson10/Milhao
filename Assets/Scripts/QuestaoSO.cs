e522744e81131ab76e3f54c45ae91d89c10fe1fa
Lauro Wadson <278412151+LauroWadson10@users.noreply.github.com>
2026-05-07 17:01:06 -0300

    commit

--- Assets/Scripts/QuestaoSO.cs
+++ Assets/Scripts/QuestaoSO.cs
@@ -5,13 +5,79 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "QuestaoSO", menuName = "Scriptable Objects/QuestaoSO")]
 public class QuestaoSO : ScriptableObject
 {
+    [TextArea(3,10)]
     [SerializeField] public string txtQuestao;
+
     [SerializeField] public List<string> opcao = new(); 
+
     [SerializeField] public NivelQuestao Nivel;
+
     [SerializeField] public Sprite[] imgQuestao = new Sprite[2];
+
     [SerializeField] public List<bool> correto = new();
+
+    [TextArea(2,8)]
     [SerializeField] public string Ajuda;
 
+    public int QuantidadeOpcoes
+    {
+        get
+        {
+            if (opcao != null)
+            {
+                return opcao.Count;
+            }
+
+            return 0;
+        }
+    }
+    public bool TemAjuda => !string.IsNullOrWhiteSpace(Ajuda);
+
+    public bool TemImagem(int indice)
+    {
+        return imgQuestao != null && indice >= 0 && indice < imgQuestao.Length && imgQuestao[indice] != null;
+    }
+
+    public bool OpcaoCorreta(int indice)
+    {
+        return correto != null && indice >= 0 && indice < correto.Count && correto[indice];
+    }
+
+    public int IndiceCorreto
+    {
+        get
+        {
+            if (correto == null)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < correto.Count; i++)
+            {
+                if (correto[i])
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+    void OnValidate()
+    {
+        opcao ??= new List<string>();
+        correto ??= new List<bool>();
+
+        while (correto.Count < opcao.Count)
+        {
+            correto.Add(false);
+        }
+
+        while (correto.Count > opcao.Count)
+        {
+            correto.RemoveAt(correto.Count - 1);
+        }
+    }
     public enum NivelQuestao
     {
         Facil,
--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -1,27 +1,117 @@
-using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using static QuestaoSO;
 
 public class QuestionManager : MonoBehaviour
 {
     [SerializeField] TMP_Text txtQuestao;
     [SerializeField] Image[] imgQuestao = new Image[2];
+
+    [SerializeField] Button[] btnOpcoes = new Button[4];
+    [SerializeField] TMP_Text[] txtOpcoes = new TMP_Text[4];
+
+    [SerializeField] TMP_Text txtAjuda;
+    [SerializeField] TMP_Text txtFeedback;
+    [SerializeField] TMP_Text txtPontuacao;
+
     [SerializeField] List<QuestaoSO> questao = new();
 
+    [SerializeField] bool construirAoIniciar = true;
+    [SerializeField] bool embaralharQuestoes = true;
+    [SerializeField] bool avancarAutomaticamente = true;
 
-    void ConstroiQuestao()
+    NivelQuestao nivelAtual = NivelQuestao.Facil;
+
+    readonly List<int> questoesDisponiveis = new();
+
+    void Start()
     {
-        int x = Random.Range(0, questao.Count);
+        if (construirAoIniciar)
+        {
+            ReiniciarJogo();
+        }
+    }
+    
+    public void ReiniciarJogo()
+    {
+        pontuacao = 0;
+        nivelAtual = NivelQuestao.Facil;
 
-        txtQuestao.text = questao[x].txtQuestao;
+        PrepararQuestoesDisponives();
+        AtualizaPontuacao();
+        ConstruirProximaQuestao();
+        
+    }
 
-        if (questao[x].imgQuestao != null)
-        {   
-            imgQuestao[0].sprite = questao[x].imgQuestao[0];
-            imgQuestao[1].sprite = questao[x].imgQuestao[1];
+    void PrepararQuestoesDisponives()
+    {
+        questoesDisponiveis.Clear();
+        if (questao == null)
+        {
+            return; 
         }
 
+        for (int i = 0; i < questao.Count; i++)
+        {
+            if (questao[i] != null)
+            {
+                questoesDisponiveis.Add(i);
+            }
+        }
+    }
+
+    int PegaQuestaoPorNivel()
+    {
+        for (int i = 0; i < questoesDisponiveis.Count; ++i)
+        {
+            int indiceQuestao = questoesDisponiveis[i];
+
+            if (questao[indiceQuestao].Nivel == nivelAtual)
+            {
+                questoesDisponiveis.RemoveAt(i);
+                return indiceQuestao;
+            }
+        }
+    }
+
+    public void SelecionarOpcao(int indiceOpcao)
+    {
+        if (questaoAtual == null)
+        {
+            return;
+        }
+
+        bool acertou = questaoAtual.EhOpcaoCorreta(indiceOpcao);
+
+        if (acertou)
+        {
+            pontuacao++;
+            AumentarDificuldade();
+        }
+
+        AtualizaPontuacao();
+        AtualizaFeedback(acertou, questaoAtual.IndiceCorreto);
+        DefineBotoesInterativos(false);
+
+        if (avancarAutomaticamente)
+        {
+            Invoke(nameof(ConstroiQuestao), Mathf.Max(0.1f, tempoParaAvancar));
+        }
+    }
+
+    void AumentarDificuldade()
+    {
+        if (nivelAtual == NivelQuestao.Facil)
+        {
+            nivelAtual = NivelQuestao.Medio;
+        }
+        else if (nivelAtual == NivelQuestao.Medio)
+        {
+            nivelAtual = NivelQuestao.Dificil;
+        }
     }
 
 
