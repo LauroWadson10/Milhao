0c5cdc38f443183b3fea2d4360fc546991510344
LauroWadson10 <lauro.silva@grupoactcon.com.br>
2026-05-08 13:23:17 -0300

    Envia indice da resposta selecionada

--- Assets/Scripts/BotaoResposta.cs
+++ Assets/Scripts/BotaoResposta.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 public class BotaoResposta : MonoBehaviour
 {
     string resposta;
+    int indiceResposta = -1;
 
     public void RegistraResposta(string resp)
     {
+        RegistraResposta(resp, -1);
+    }
+
+    public void RegistraResposta(string resp, int indice)
+    {
         resposta = resp;
-        GetComponentInChildren<TMP_Text>().text = resp;
+        indiceResposta = indice;
+
+        TMP_Text texto = GetComponentInChildren<TMP_Text>();
+
+        if (texto != null)
+        {
+            texto.text = resp;
+        }
     }
 
     public void Responde()
     {
+        if (QuestionManager.instance == null)
+        {
+            return;
+        }
+
+        if (indiceResposta >= 0)
+        {
+            QuestionManager.instance.ConfirmaResposta(indiceResposta);
+            return;
+        }
+
         QuestionManager.instance.ConfirmaResposta(resposta);
     }
 }
